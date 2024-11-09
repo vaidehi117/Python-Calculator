0778932347de154ee6b11da4e389ba7b1d2b0c1f
vaidehi <vaidehi117v@gmail.com>
2024-11-08 21:33:18 -0500

    calculator operations

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,14 +1,29 @@
-
 def addition(a: float, b: float) -> float:
+    """
+    Returns:
+    float: The result of adding `a` and `b`.
+    """
     return a + b
 
 def subtraction(a: float, b: float) -> float:
+    """
+    Returns:
+    float: The result of subtracting `b` from `a`.
+    """
     return a - b
 
 def multiplication(a: float, b: float) -> float:
+    """
+    Returns:
+    float: The result of multiplying `a` by `b`.
+    """
     return a * b 
 
 def division(a: float, b: float) -> float:
+    """
+    Returns:
+    float: The result of dividing `a` by `b`.
+    """
     if b == 0:
-        raise ValueError("Division by zero is not allowed.")  
+        raise ValueError("Division by zero is not allowed.")  # Guard against division by zero
     return a / b
